5e8b621ccf0433a5692f80a34bdcf91761fe6fb1
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-15 01:15:07 -0400

    Added print name definitions and print statements

---
+++ JavaProjects/hello/HelloGoodbye.java
@@ -0,0 +1,9 @@
+public class HelloGoodbye {
+    public static void main(String argc[]) {
+        string name1 = argc[0];
+        string name2 = argc[1];
+
+        System.out.println("Hello " + name1 + " and " + name2 + ".");
+        System.out.println("Goodbye " + name2 + " and " + name1 + ".");
+    }
+}
