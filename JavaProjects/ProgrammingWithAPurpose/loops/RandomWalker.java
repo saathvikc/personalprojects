fab57ebc8d6f3984f91bb4d40c39d5ad666ea2df
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 18:42:22 -0400

    initialized variables and loop

--- JavaProjects/ProgrammingWithAPurpose/loops/RandomWalker.java
+++ JavaProjects/ProgrammingWithAPurpose/loops/RandomWalker.java
@@ -1,5 +1,12 @@
 public class RandomWalker {
     public static void main(String[] args) {
-        
+        int r = Integer.parseInt(args[0]);
+        int num_steps = 0;
+        int x1 = 0, x2 = 0;
+        int y1 = 0, y2 = 0;
+
+        while (Math.abs(x1 - x2) + Math.abs(y1 - y2) != r) {
+            
+        }
     }
 }
