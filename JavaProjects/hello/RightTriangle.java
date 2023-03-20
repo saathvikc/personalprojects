ef07ab747b0afadfac55d0ae3cf44b56d9d83efd
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 00:13:17 -0400

    Working right triangle program

--- JavaProjects/hello/RightTriangle.java
+++ JavaProjects/hello/RightTriangle.java
@@ -1,5 +1,12 @@
 public class RightTriangle {
     public static void main(String[] args) {
-        
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int c = Integer.parseInt(args[2]);
+
+        boolean positive = (a > 0 && b > 0 && c > 0);
+        boolean right = ((Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) || (Math.pow(a, 2) + Math.pow(c, 2) == Math.pow(b, 2)) || (Math.pow(c, 2) + Math.pow(b, 2) == Math.pow(a, 2)));
+
+        System.out.println(positive && right);
     }
 }
