9603b152c757e02d05780ece50c32a00d730ef17
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 18:31:58 -0400

    trying to figure out

--- JavaProjects/ProgrammingWithAPurpose/loops/BandMatrix.java
+++ JavaProjects/ProgrammingWithAPurpose/loops/BandMatrix.java
@@ -2,14 +2,18 @@ public class BandMatrix {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         int width = Integer.parseInt(args[1]);
-        String[][] arr = new String[n][n];
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i == j) {
                     
+                } else if (j != n) {
+                    System.out.print("0 ");
+                } else {
+                    System.out.print("0");
                 }
             }
+            System.out.println();
         }
     }
 }
