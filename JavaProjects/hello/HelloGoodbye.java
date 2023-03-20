479df317be2b66054748c6b8a2be0fbc3ca8f2af
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-19 23:58:43 -0400

    made changes

--- JavaProjects/hello/HelloGoodbye.java
+++ JavaProjects/hello/HelloGoodbye.java
@@ -1,7 +1,7 @@
 public class HelloGoodbye {
     public static void main(String[] args) {
-        string name1 = argc[0];
-        string name2 = argc[1];
+        String name1 = args[0];
+        String name2 = args[1];
 
         System.out.println("Hello " + name1 + " and " + name2 + ".");
         System.out.println("Goodbye " + name2 + " and " + name1 + ".");
