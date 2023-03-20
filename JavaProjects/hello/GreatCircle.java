482b0b73d440cd4a5cb760fc3820a5e0c1d1359b
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 00:44:08 -0400

    minor fix

--- JavaProjects/hello/GreatCircle.java
+++ JavaProjects/hello/GreatCircle.java
@@ -1,15 +1,15 @@
 public class GreatCircle {
     public static void main(String[] args) {
-        Double x1 = Math.toRadians(Double.parseDouble(args[0]));
-        Double y1 = Math.toRadians(Double.parseDouble(args[1]));
-        Double x2 = Math.toRadians(Double.parseDouble(args[2]));
-        Double y2 = Math.toRadians(Double.parseDouble(args[3]));
-        Double r = 6371.0;
+        double x1 = Math.toRadians(Double.parseDouble(args[0]));
+        double y1 = Math.toRadians(Double.parseDouble(args[1]));
+        double x2 = Math.toRadians(Double.parseDouble(args[2]));
+        double y2 = Math.toRadians(Double.parseDouble(args[3]));
+        double r = 6371.0;
 
-        Double sinx = Math.pow(Math.sin((x2 - x1) / 2), 2);
-        Double siny = Math.pow(Math.sin((y2 - y1) / 2), 2);
+        double sinx = Math.pow(Math.sin((x2 - x1) / 2), 2);
+        double siny = Math.pow(Math.sin((y2 - y1) / 2), 2);
         Double root = Math.sqrt(sinx + (Math.cos(x1) * Math.cos(x2) * siny));
-        Double distance = 2 * r * Math.asin(root);
+        double distance = 2 * r * Math.asin(root);
 
         System.out.println(distance + " kilometers");
     }
