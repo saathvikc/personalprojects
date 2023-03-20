084cda7d16adf0a60e7bde4394c74c1932134015
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 00:45:03 -0400

    minor fixes

--- JavaProjects/hello/CMYKtoRGB.java
+++ JavaProjects/hello/CMYKtoRGB.java
@@ -1,14 +1,14 @@
 public class CMYKtoRGB {
     public static void main(String[] args) {
-        Double cyan = Double.parseDouble(args[0]);
-        Double magenta = Double.parseDouble(args[1]);
-        Double yellow = Double.parseDouble(args[2]);
-        Double black = Double.parseDouble(args[3]);
+        double cyan = Double.parseDouble(args[0]);
+        double magenta = Double.parseDouble(args[1]);
+        double yellow = Double.parseDouble(args[2]);
+        double black = Double.parseDouble(args[3]);
 
-        Double white = 1 - black;
-        int red =  (int) (255 * white * (1 - cyan));
-        int green =  (int) (255 * white * (1 - magenta));
-        int blue =  (int) (255 * white * (1 - yellow));
+        double white = 1 - black;
+        int red = (int) Math.round(255 * white * (1 - cyan));
+        int green = (int) Math.round(255 * white * (1 - magenta));
+        int blue =  (int) Math.round(255 * white * (1 - yellow));
 
         System.out.println("red   = " + red);
         System.out.println("green = " + green);
