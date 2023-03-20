bf5e028c9290915dafcbf17de82e37d61e87e91b
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 17:41:50 -0400

    initialized variables and loops

--- JavaProjects/ProgrammingWithAPurpose/loops/BandMatrix.java
+++ JavaProjects/ProgrammingWithAPurpose/loops/BandMatrix.java
@@ -1,5 +1,15 @@
 public class BandMatrix {
     public static void main(String[] args) {
-        
+        int n = Integer.parseInt(args[0]);
+        int width = Integer.parseInt(args[1]);
+        String[][] arr = new String[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) {
+                    
+                }
+            }
+        }
     }
 }
