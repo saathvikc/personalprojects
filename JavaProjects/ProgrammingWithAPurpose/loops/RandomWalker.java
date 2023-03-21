d7a8bf82e5064f63d322ab1d0ad44682aa5b227d
Saathvik <saathvik.chandupatla@gmail.com>
2023-03-20 22:01:16 -0400

    first version

--- JavaProjects/ProgrammingWithAPurpose/loops/RandomWalker.java
+++ JavaProjects/ProgrammingWithAPurpose/loops/RandomWalker.java
@@ -6,7 +6,21 @@ public class RandomWalker {
         int y1 = 0, y2 = 0;
 
         while (Math.abs(x1 - x2) + Math.abs(y1 - y2) != r) {
+            double direction = Math.random();
+            if (direction < 0.25) {
+                y1++;
+            } else if (direction < 0.5) {
+                y1--;
+            } else if (direction < 0.75) {
+                y2++;
+            } else {
+                y2--;
+            }
             
+            System.out.println("(" + y1 + ", " + y2 + ")");
+            num_steps++;
         }
+
+        System.out.println("steps = " + num_steps);
     }
 }
